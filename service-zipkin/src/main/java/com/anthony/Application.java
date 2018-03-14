e6c33078d42cc74a6b3fef6942b3bd19c92c6561
Anthony <cdistc@163.com>
2018-03-14 17:58:56 +0800

    first commit

--- service-zipkin/src/main/java/com/anthony/Application.java
+++ service-zipkin/src/main/java/com/anthony/Application.java
@@ -1,12 +1,12 @@
-//package com.anthony;
-//
-//import org.springframework.boot.SpringApplication;
-//
-//
-//public class Application {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-//    }
-//}
-//
+package com.anthony;
+
+import org.springframework.boot.SpringApplication;
+
+//@EnableZipkinServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
