15911efe5efb2919342dda514be1848d41cc369a
Anthony <cdistc@163.com>
2018-03-14 14:17:53 +0800

    first commit

---
+++ biz-helloworld/src/main/java/com/anthony/Application.java
@@ -0,0 +1,25 @@
+package com.anthony;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableEurekaClient
+@SpringBootApplication
+@RestController
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hello")
+    public String helloWorld() {
+        return "helloWorld:" + port;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+service-eureka-server 服务注册
+service-zuul 路由网关
+service-config 配置中心
